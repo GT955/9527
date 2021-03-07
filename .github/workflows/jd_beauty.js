667e9c4d38e241a639b2db3109ee4de39116c639
GT955 <74489846+GT955@users.noreply.github.com>
2021-03-07 13:45:06 +0800

    Update jd_beauty.js

--- .github/workflows/jd_beauty.js
+++ .github/workflows/jd_beauty.js
@@ -20,8 +20,6 @@ jobs:
       - name: Checkout
         run: |
           git clone -b main https://github.com/GT977/F1.git ~/F1
-      - name: Checkout
-        uses: actions/checkout@v2
       - name: Use Node.js ${{ matrix.node-version }}
         uses: actions/setup-node@v1
         with:
